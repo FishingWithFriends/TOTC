bcf4ff407d21ccb0ec35c98ba4f081886684c5dd
Felix Hu <felixhu12@gmail.com>
2014-04-04 23:03:34 -0500

    modified server files

--- bin/main.dart
+++ bin/main.dart
@@ -12,12 +12,7 @@ main(args) {
     new Directory('build').exists().then((exists) {
       if(exists) {
         new Router(server)
-          ..serve('/').listen(serveFile('build/rpghelper.html'))
-          ..serve('/rpghelper.css').listen(serveFile('build/rpghelper.css'))
-          ..serve('/packages/shadow_dom/shadow_dom.debug.js').listen(serveFile('build/packages/shadow_dom/shadow_dom.debug.js'))
-          ..serve('/packages/custom_element/custom-elements.debug.js').listen(serveFile('build/packages/custom_element/custom-elements.debug.js'))
-          ..serve('/packages/browser/interop.js').listen(serveFile('build/packages/browser/interop.js'))
-          ..serve('/rpghelper.html_bootstrap.dart.js').listen(serveFile('build/rpghelper.html_bootstrap.dart.js'))
+          ..serve('/').listen(serveFile('web/index.html'))
           ;
       } else {
         new Router(server)
